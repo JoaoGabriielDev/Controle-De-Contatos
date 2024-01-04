507e813708150815a5a88bba92c7baa1376e7266
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-01-03 22:03:55 -0300

    Funcionalidades de Editar e Deletar implementado

--- Repositorio/IContatoRepositorio.cs
+++ Repositorio/IContatoRepositorio.cs
@@ -8,9 +8,11 @@ namespace ControleContatos.Repositorio;
 
     public interface IContatoRepositorio 
     {
-
         List<ContatoModel> BuscarTodos();
         ContatoModel Adicionar(ContatoModel contato);
+        ContatoModel ListarPorId(int id);
+        ContatoModel Atualizar(ContatoModel contato);
+        bool Apagar(int id);
     }
 
 
